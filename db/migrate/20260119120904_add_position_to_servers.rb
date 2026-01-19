2fc25cba3dad8960a999e2fd9ecd5aced95d0ad3
Max <maximz2009@gmail.com>
2026-01-19 17:23:23 +0300

    Harden migrations and add CI smoke test

--- db/migrate/20260119120904_add_position_to_servers.rb
+++ db/migrate/20260119120904_add_position_to_servers.rb
@@ -1,5 +1,7 @@
 class AddPositionToServers < ActiveRecord::Migration[8.0]
   def change
+    return if column_exists?(:servers, :position)
+
     add_column :servers, :position, :integer, default: 0, null: false
   end
 end
--- db/migrate/20260119125508_create_app_settings.rb
+++ db/migrate/20260119125508_create_app_settings.rb
@@ -1,12 +1,12 @@
 class CreateAppSettings < ActiveRecord::Migration[8.0]
   def change
-    create_table :app_settings do |t|
+    create_table :app_settings, if_not_exists: true do |t|
       t.string :key
       t.string :value
 
       t.timestamps
     end
 
-    add_index :app_settings, :key, unique: true
+    add_index :app_settings, :key, unique: true, if_not_exists: true
   end
 end
